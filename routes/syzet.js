2d3782d9a2fe3adedc3cf86621d0db7d4a320670
USER1-PC\user <a.gjini@live.com>
2017-07-13 12:38:17 +0200

    optike connected with magazina

--- routes/syzet.js
+++ routes/syzet.js
@@ -132,7 +132,7 @@ exports.findAllOptike = function (req, res, next) {
       console.log('Connected to postgres! 4');
 
       client
-        .query('SELECT DISTINCT ON (products2.pershkrimiangartikulli) * FROM products2 INNER JOIN cmime2 ON (products2.kodartikulli=cmime2.idprodukti) WHERE products2.grupi=\'Syze Optike\'  limit 20 offset ' + offset)
+        .query('SELECT DISTINCT ON (products2.pershkrimiangartikulli) * FROM products2 INNER JOIN cmime2 ON (products2.kodartikulli=cmime2.idprodukti) INNER JOIN magazina ON (products2.kodartikulli=magazina.kodartikull) WHERE magazina.sasia>0 AND WHERE products2.grupi=\'Syze Optike\'  limit 20 offset ' + offset)
         .on('row', function(row) {
           syzetO.push(row);
           console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
